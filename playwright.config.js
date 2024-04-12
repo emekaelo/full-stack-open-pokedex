6341c722771ffba83b2df17bd6f1ca634574b8c6
emekaelo <elochux@gmail.com>
2024-04-12 04:32:19 +0100

    Exercise 11.9: fix lint errors

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test')
 
 /**
  * Read environment variables from file.
@@ -75,5 +75,5 @@ module.exports = defineConfig({
     url: 'http://localhost:8081/',
     reuseExistingServer: !process.env.CI,
   },
-});
+})
 
