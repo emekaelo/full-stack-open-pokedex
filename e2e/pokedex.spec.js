41924c1eb1d31f99accc1bbb09f7e1d9ae490712
emekaelo <elochux@gmail.com>
2024-04-12 04:23:44 +0100

    Exercise 11.9: Simple end-to-end tests completed

---
+++ e2e/pokedex.spec.js
@@ -0,0 +1,10 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Pokedex', () => {
+    test('front page can be opened', async ({ page }) => {
+        await page.goto('http://localhost:8081/')
+        await expect(page.getByText('ivysaur')).toBeVisible()
+        await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+    })
+})
